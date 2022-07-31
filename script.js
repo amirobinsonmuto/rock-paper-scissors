67894482a2ca212a11bdadd66db44bb749af23c8
AmiRobinsonMuto <amimuto1002@gmail.com>
2022-07-31 15:43:16 -0700

    Add UI with three buttons to work with eventlistners.

--- script.js
+++ script.js
@@ -1,80 +1,133 @@
 
-//Set variables for points to start with 0
-let playerPoint = 0;
-let computerPoint = 0;
+    
+//global functions
 
-function game() {
+    let playerPoint = 0;
+    let computerPoint = 0;
+    let computerSelection =""
+    const messageStart = "Click on rock paper or scissors!"
+    const messageTie = "It's a tie in this round!";
+    const messageWin = "You won in this round!";
+    const messageLose = 'You lost in this round!';
+    const messageGameEndWin = 'You won this entire game! Play again by clicking the refresh button.';
+    const messageGameEndLost = 'You lost this entire game! Try again by clicking the refresh button.';
 
-    function playRound() {
+//functions to call
 
-        // Computer randomly returns either 'rock', 'paper', 'scissors'
-        function computerPlay() {
-            const computerArray = ['rock', 'paper', 'scissors']
-            return computerArray[Math.floor (Math.random() *3)];
-            }
-
-        const computerSelection = computerPlay();
-        const playerInitialSelection = prompt('Rock Paper or Scissors?', ' ');
-        const playerSelection = playerInitialSelection.toLowerCase();
-
-        //If statements for all possible combinations.
-
-        //playerSelection === 'rock'
-        if (playerSelection === 'rock' && computerSelection === 'rock') {
-            return 'Tie!';
-
-        } else if (playerSelection === 'rock' && computerSelection === 'paper'){
-            computerPoint++
-            return 'Lose!';
-
-        } else if (playerSelection === 'rock' && computerSelection === 'scissors'){
-            playerPoint++
-            return 'Win!';
-        }
-
-        //playerSelection === 'paper'
-        if (playerSelection === 'paper' && computerSelection === 'rock') {
-            playerPoint++
-            return 'Win!';
-
-        } else if (playerSelection === 'paper' && computerSelection === 'paper'){
-            return 'Tie!';
-
-        } else if (playerSelection === 'paper' && computerSelection === 'scissors'){
-            computerPoint++
-            return 'Lose!';
-        }
-
-        //playerSelection === 'scissors'
-        if (playerSelection === 'scissors' && computerSelection === 'rock') {
-            computerPoint++
-            return 'Lose!';
-
-        } else if (playerSelection === 'scissors' && computerSelection === 'paper'){
-            playerPoint++
-            return 'Win!';
-
-        } else if (playerSelection === 'scissors' && computerSelection === 'scissors'){
-            return 'Tie!';
-        }
+    function computerPlay() {
+    const computerArray = ['rock', 'paper', 'scissors']
+    return computerArray[Math.floor (Math.random() *3)];
     }
 
-    //Show result in console
-        console.log(playRound());
-        console.log(computerPoint);
-        console.log(playerPoint);
-}
+    function endEvent () {
+        if (playerPoint === 5) {
++
+            //make the buttons unclickable. Currently hiding the buttons. 
 
-//Play the function game until either player or computer to reach 5 points.
-do {
-    game()
-} while(computerPoint < 5 && playerPoint < 5);
+            document.getElementById("rock").classList.add('hidden');
+            document.getElementById("paper").classList.add('hidden');
+            document.getElementById("scissors").classList.add('hidden');
 
-//Console log the final winner when either player or computer reached 5 points.
-if (computerPoint === 5) {
-    console.log('You got 5 points! You win!');
-} else {
-    console.log('Computer wins! Try again!');
-}
+            document.getElementById("message").textContent = messageGameEndWin;
+            document.getElementById("refresh").classList.remove('hidden');
 
+        } else if (computerPoint === 5) {
 
+            //make the button unclikable. Currently hiding the buttons.
+
+            document.getElementById("rock").classList.add('hidden');
+            document.getElementById("paper").classList.add('hidden');
+            document.getElementById("scissors").classList.add('hidden');
+
+            document.getElementById("message").textContent = messageGameEndLost;
+            
+            document.getElementById("refresh").classList.remove('hidden');
+            
+        } else {
+            //do nothing
+        }
+    }
+    
+    function refresh () {
+                playerPoint = 0;
+                computerPoint = 0;
+    
+                document.getElementById("message").textContent = messageStart;
+        
+                document.getElementById("playerPointDisplay").textContent = `Player Point is ${playerPoint}`;
+                document.getElementById("computerPointDisplay").textContent = `Computer Point is ${computerPoint}`;
+        
+                document.getElementById("rock").classList.remove('hidden');
+                document.getElementById("paper").classList.remove('hidden');
+                document.getElementById("scissors").classList.remove('hidden');
+
+                document.getElementById("refresh").classList.add('hidden');
+    }
+
+//main function 
+
+    const btnRock = document.querySelector('#rock');
+    btnRock.addEventListener('click', () => {
+
+        computerSelection = computerPlay();
+        console.log(computerSelection);
+
+        if(computerSelection === 'rock') {
+            document.getElementById("message").textContent = messageTie;
+        } else if(computerSelection === 'paper') {
+            document.getElementById("message").textContent = messageLose;
+            computerPoint++;
+        } else {
+            document.getElementById("message").textContent = messageWin;
+            playerPoint++;
+        }
+
+        document.getElementById("playerPointDisplay").textContent = `Player Point is ${playerPoint}`;
+        document.getElementById("computerPointDisplay").textContent = `Computer Point is ${computerPoint}`;
+        endEvent();
+    })
+
+    const btnPaper = document.querySelector('#paper');
+    btnPaper.addEventListener('click', () => {
+
+        computerSelection = computerPlay();
+        console.log(computerSelection);
+
+        if(computerSelection === 'rock') {
+            document.getElementById("message").textContent = messageWin;
+            playerPoint++;
+        } else if(computerSelection === 'paper') {
+            document.getElementById("message").textContent = messageTie;
+        } else {
+            document.getElementById("message").textContent = messageLose;
+            computerPoint++;
+        }
+
+        document.getElementById("playerPointDisplay").textContent = `Player Point is ${playerPoint}`;
+        document.getElementById("computerPointDisplay").textContent = `Computer Point is ${computerPoint}`;
+        endEvent();
+    })
+
+    const btnScissors = document.querySelector('#scissors');
+    btnScissors.addEventListener('click', () => {
+
+        computerSelection = computerPlay();
+        console.log(computerSelection);
+
+        if(computerSelection === 'rock') {
+            document.getElementById("message").textContent = messageLose;
+            computerPoint++;
+        } else if(computerSelection === 'paper') {
+            document.getElementById("message").textContent = messageWin;
+            playerPoint++;
+        } else {
+            document.getElementById("message").textContent = messageTie;
+        }
+
+        document.getElementById("playerPointDisplay").textContent = `Player Point is ${playerPoint}`;
+        document.getElementById("computerPointDisplay").textContent = `Computer Point is ${computerPoint}`;
+        endEvent();
+    })
+
+    const refreshBtn = document.querySelector('#refresh');
+    refreshBtn.addEventListener('click', refresh);
